e145e30f97befc4647b3c6600e52fed103be79ce
Bing Wu <wub@ijet.com>
2017-07-10 20:35:22 -0400

    updated POM with JUnit dependency.

--- src/main/java/com/bingmwu/edge/sto/fast/FastStoController.java
+++ src/main/java/com/bingmwu/edge/sto/fast/FastStoController.java
@@ -1,5 +1,7 @@
 package com.bingmwu.edge.sto.fast;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -11,6 +13,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bingmwu.ApplicationMain;
+import com.bingmwu.analyzer.FastStochAnalyzer;
+import com.bingmwu.analyzer.data.StochasticData;
+import com.bingmwu.data.DataCombo;
+import com.bingmwu.data.FileSystemDataLoader;
 
 @RestController
 @RequestMapping(value = ApplicationMain.BASE_RELATIVE_API_URL)
@@ -19,9 +25,13 @@ public class FastStoController {
 	private static final Logger logger = LoggerFactory.getLogger(FastStoController.class);
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public String getIntel(HttpServletRequest request,
-			@PathVariable(value = "id") int id) {
+	public String getEdge(HttpServletRequest request, @PathVariable(value = "id") int id) {
 		logger.info("intel={}", id);
+
+		FileSystemDataLoader dataLoader = new FileSystemDataLoader("C:\\dev-app\\genotick\\yahoo_spx_data\\spx_d.csv");
+		DataCombo dataCombo = dataLoader.loadData();
+		new FastStochAnalyzer(
+				FastStochAnalyzer.STOCHASTIC_PERIOD_14_DAYS_BY_DEFAULT, dataCombo.dailyDataList).analyze();
 		return "test";
 
 	}
